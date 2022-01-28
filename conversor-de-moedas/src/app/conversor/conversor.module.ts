e3ab05eb79d6fb92a2cee352f95e504d51125b1d
Lucas Gama <lucasgdon@gmail.com>
2022-01-27 22:56:44 -0300

    Criação da diretiva numero + importação no modulo

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
+import { NumeroDirective } from './directives';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
